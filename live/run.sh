0d8650e8bc1602851f15728667a9db5631554a8b
Y M <yaseengem@gmail.com>
2024-10-01 12:30:29 +0000

    fixed the error.

--- live/run.sh
+++ live/run.sh
@@ -1,4 +1,24 @@
+
+#!/bin/bash
 # run this file using "sudo ./run.sh"
+
+# remove all existing containers
+# sudo docker stop $(sudo docker ps -q)
+# sudo docker rm -f $(sudo docker ps -aq)
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+# Check if ENV variable is set
+if [ -z "$ENV" ]; then
+  echo "ENV variable is not set in the .env file. Defaulting to 'dev'."
+  ENV="dev"
+fi
+
+echo "Environment: $ENV"
+
 
 export JAVA_HOME=/opt/jdk-23
 export PATH=$JAVA_HOME/bin:$PATH
@@ -6,18 +26,14 @@ export PATH=$JAVA_HOME/bin:$PATH
 export MAVEN_HOME=/opt/apache-maven-3.9.9
 export PATH=$MAVEN_HOME/bin:$PATH
 
-
 # rm -rf target/
 
 mvn clean package -DskipTests
 # mvn package -DskipTests
 
-
 # Enable debug logging for Docker Compose
 # export COMPOSE_LOG_LEVEL=DEBUG
 
-# Build and start the Docker containers
+# Build and start the Docker containers with the specified environment
 # sudo docker-compose up --build
 docker compose up --build
-
-
