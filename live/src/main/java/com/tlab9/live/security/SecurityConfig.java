5b86a0c70f37a4533453ff8de894cce5b9df2bba
Y M <yaseengem@gmail.com>
2024-10-13 21:07:22 +1100

    Added localhost to CORS

--- live/src/main/java/com/tlab9/live/security/SecurityConfig.java
+++ live/src/main/java/com/tlab9/live/security/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -22,45 +24,31 @@ public class SecurityConfig {
     @Autowired
     private ApiKeyFilter apiKeyFilter;
 
-
     @Autowired
     private GoogleTokenFilter googleTokenFilter;
 
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-    // Exception {
-    // http
-    // .authorizeHttpRequests(authz -> authz
-    // .requestMatchers(
-    // "/v3/api-docs",
-    // "/swagger-ui/**"
-    // ).permitAll()
-    // .anyRequest().authenticated()
-    // )
-    // .addFilterBefore(apiKeyFilter, UsernamePasswordAuthenticationFilter.class)
-    // .exceptionHandling(exceptionHandling -> exceptionHandling
-    // .authenticationEntryPoint((request, response, authException) ->
-    // response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-    // )
-    // .csrf(csrf -> csrf.disable());
-
-    // return http.build();
-    // }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // Adjust the port if necessary
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        
-        http.csrf(csrf -> csrf.disable())
+        http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                        // .requestMatchers("/api/subjects").permitAll()
-                        // .requestMatchers("/api/subjects/**").permitAll()
-                        // .requestMatchers("/swagger-ui.html").permitAll()
                         // .requestMatchers("/swagger-ui/**").permitAll()
                         // .requestMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(googleTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(apiKeyFilter, UsernamePasswordAuthenticationFilter.class)
- 
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint((request, response, authException) -> {
                             log.warn("Unauthorized request - {}", authException.getMessage());
@@ -69,12 +57,25 @@ public class SecurityConfig {
                         .accessDeniedHandler((request, response, accessDeniedException) -> {
                             log.warn("Access denied - {}", accessDeniedException.getMessage());
                             response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
-                        }));
+                        }))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())); // Apply the CORS configuration
+
         return http.build();
     }
 
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:*"); // Adjust the port if necessary
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
     // @Bean
     // public WebSecurityCustomizer webSecurityCustomizer() {
-    //     return (web) -> web.ignoring().requestMatchers("/public/**");
+    // return (web) -> web.ignoring().requestMatchers("/public/**");
     // }
 }
