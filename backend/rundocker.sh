8e04d2e81408395661834bc430b31036dea3984a
Yaseen M <yaseengem@gmail.com>
2025-03-09 03:30:57 +1100

    ..

--- backend/rundocker.sh
+++ backend/rundocker.sh
@@ -1,67 +1,22 @@
-
-
-
 #!/bin/bash
 
-# Run this file using "sudo ./run.sh"
-# This script is used to build and run the application in a Docker container.
+# Exit on error
+set -e
 
-# remove all existing containers
-# sudo docker stop $(sudo docker ps -q)
-# sudo docker rm -f $(sudo docker ps -aq)
-
-# Load environment variables from .env file
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
-fi
-
-# Check if ENV variable is set
-if [ -z "$ENV" ]; then
-  echo "ENV variable is not set in the .env file. Defaulting to 'dev'."
-  ENV="dev"
-fi
-
-echo "Environment: $ENV"
-
-
-export JAVA_HOME=/opt/jdk-23
-export PATH=$JAVA_HOME/bin:$PATH
-
-export MAVEN_HOME=/opt/apache-maven-3.9.9
-export PATH=$MAVEN_HOME/bin:$PATH
-
-
-if [ "$ENV" = "dev" ]; then
-  echo "Free disk space info before cleaning:"
-  df -h --total | awk '/total/ {print "Total: " $2 ", Used: " $3 ", Free: " $4}'
-  echo "Stopping and removing containers..."
-  docker compose down
-  echo "Removing existing pgdata volume..."
-  docker volume ls
-  docker volume rm -f live_pgdata
-  docker volume ls
-#   echo "Clear docker cache"
-#   docker system prune -a -f
-#   docker system prune -a --volumes -f
-  # echo "Remove Unused docker images"
-  # docker image prune -a -f 
-  # echo "Remove Unused docker containers, volumes, networks"
-  # docker container prune -f
-  docker volume prune -f
-  docker network prune -f
-  echo "Free disk space info after cleaning:"
-  df -h --total | awk '/total/ {print "Total: " $2 ", Used: " $3 ", Free: " $4}'
-fi
-
-
-# rm -rf target/
-
+# Step 1: Build the Java application
+echo "Building Java application..."
 mvn clean package -DskipTests
-# mvn package -DskipTests
 
-# Enable debug logging for Docker Compose
-# export COMPOSE_LOG_LEVEL=DEBUG
+# Step 2: Build the Docker image
+echo "Building Docker image..."
+docker build -t track_app .
 
-# Build and start the Docker containers with the specified environment
-# sudo docker-compose up --build
-docker compose up --build
+# Step 3: Stop and remove existing containers
+echo "Stopping and removing existing containers..."
+docker compose down
+
+# Step 4: Start new containers
+echo "Starting new containers..."
+docker compose up -d --build
+
+echo "Deployment completed successfully!"
